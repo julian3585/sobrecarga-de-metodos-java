f5ca3ba9596a73f77c09f583d9d0b794dec79c64
julian3585 <107727983+julian3585@users.noreply.github.com>
2022-06-22 18:56:05 -0500

    Add files via upload

---
+++ edad.java
@@ -0,0 +1,30 @@
+public class edad {
+
+    //atributos//
+    public int edadActual;
+    public int edadFutura;
+    public int años;
+
+
+    //constructor//
+    public edad (String s){
+        this.edadActual = edadActual;
+        this.años = años;
+
+    }
+
+
+    public edad(int edadFutura){
+        this.edadFutura = edadFutura;
+    }
+
+
+        public void edadFutura(){
+        System.out.println("mi edada actua es " + edadActual + "años" + años);
+        }
+
+            public void edadFutura(int edadFutura){
+            System.out.println("mi edad futura es " + edadFutura);
+
+            }
+}
---
+++ main.java
@@ -0,0 +1,15 @@
+public class main {
+
+
+    public  main(){
+
+    }
+
+    public static void main(String [] args){
+        edad edadActual = new edad ("edad actual");
+        edad años = new edad("años transcurridos");
+        años.edadFutura(21);
+
+    }
+
+}
